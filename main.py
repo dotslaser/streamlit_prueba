1c10a5d9e2cd6e75cccbde3d103d7249e951cec8
Ignacio RG <97739554+dotslaser@users.noreply.github.com>
2022-01-14 14:57:08 +0000

    Add table

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import streamlit as st
+import pandas as pd
+
+data = {
+  'Series':[1,3,4,5,7],
+  'Series2':[10,30,40,100,250]
+}
+
+df = pd.DataFrame(data)
 
 st.title('App')
 st.subheader('Introduction')
 st.write('First App')
+
+st.write(df)
